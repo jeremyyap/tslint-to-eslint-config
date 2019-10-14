67c0b158b7d68a04d3b1712c7d6ca0f468cd0910
Jeremy Yap <dcsyjlj@nus.edu.sg>
2019-10-14 16:53:36 +0800

    Use ESLint functions to lookup config

--- src/input/findESLintConfiguration.ts
+++ src/input/findESLintConfiguration.ts
@@ -1,6 +1,10 @@
+import * as rewire from "rewire";
+import * as path from "path";
+import * as fs from "fs";
+
 import { ESLintRuleSeverity } from "../rules/types";
 import { TSLintToESLintSettings } from "../types";
-import { findConfiguration, FindConfigurationDependencies } from "./findConfiguration";
+import { FindConfigurationDependencies } from "./findConfiguration";
 
 export type ESLintConfiguration = {
     env: {
@@ -31,11 +35,9 @@ export const findESLintConfiguration = async (
     dependencies: FindConfigurationDependencies,
     rawSettings: Pick<TSLintToESLintSettings, "config" | "eslint">,
 ): Promise<ESLintConfiguration | Error> => {
-    const rawConfiguration = await findConfiguration<ESLintConfiguration>(
-        dependencies.exec,
-        "eslint --print-config",
-        rawSettings.eslint || rawSettings.config,
-    );
+    const rawConfiguration = loadConfig();
+
+    console.log(rawConfiguration);
 
     return rawConfiguration instanceof Error
         ? rawConfiguration
@@ -44,3 +46,27 @@ export const findESLintConfiguration = async (
               ...rawConfiguration,
           };
 };
+
+function loadConfig() {
+    const ConfigArrayFactory = rewire("eslint/lib/cli-engine/config-array-factory");
+    const loadConfigFile = ConfigArrayFactory.__get__("loadConfigFile");
+    const configFilenames = ConfigArrayFactory.__get__("configFilenames");
+
+    for (const filename of configFilenames) {
+        const filePath = path.join(process.cwd(), filename);
+
+        if (fs.existsSync(filePath)) {
+            let configData;
+
+            try {
+                configData = loadConfigFile(filePath);
+            } catch (error) {
+                return error;
+            }
+
+            if (configData) {
+                return configData;
+            }
+        }
+    }
+}
